5cf99657b81842f1763d1a963627a70f7e26e3db
Lang Feng <feng2011312488@163.com>
2015-09-13 23:44:46 +0800

    Update plot2.R

--- plot2.R
+++ plot2.R
@@ -4,5 +4,5 @@ data$Date <- as.Date(data$Date, format = "%d/%m/%Y") ## change the format of Dat
 data1<-subset(data,data$Date=="2007-02-01"|data$Date=="2007-02-02") ## subsetting
 DateTime<-paste(data1$Date,data1$Time)
 DateTime <- strptime(DateTime, format = "%Y-%m-%d %H:%M:%S")  ## set the format of the DateTime to be format Date
-plot(DateTime,data1$Global_active_power,type = "l",xlab="",ylab = "Global_Active_Power(Killowatts)")
+plot(DateTime,data1$Global_active_power,type = "l",xlab="",ylab = "Global Active Power(Killowatts)")
 dev.copy(png,file="plot2.png")  ## save the png to the file plot2.png
